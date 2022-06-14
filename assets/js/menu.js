4b8b5d9f3eaf56bca76dad9893045e77697070ac
aki0921 <66670058+aki0921@users.noreply.github.com>
2022-06-14 22:10:09 +0900

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,5 @@
+document.getElementById('navBtn').addEventListener('click' , function() {
+  document.getElementById('line1').classList.toggle('rotate1');
+  document.getElementById('line2').classList.toggle('rotate2');
+  document.getElementById('nav').classList.toggle('show');
+})
